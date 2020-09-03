4666405f2011bbf9f34bc894d71eab027b84a1d7
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-09-03 18:44:40 -0400

    added movement script

---
+++ Assets/Input/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]private int spd; //modifier so for the ball
+    [SerializeField] private float lmt = 5; //speed limit 
+    private Rigidbody rb;
+    private float x;
+    private float y; //direction on which force is applied
+    private Vector3 moving;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+    void FixedUpdate()
+    {
+        Vector3 move = new Vector3(x, 0.0f, y);
+        moving += move;
+        if (moving.x <lmt || moving.z < lmt)
+        {
+            rb.AddForce(moving *spd *Time.deltaTime);
+        }
+        
+    }
+    void OnMove(InputValue movementVal)
+    {
+        Vector2 movementVec = movementVal.Get<Vector2>();
+        x = movementVec.x;
+        y = movementVec.y;
+    }
+}
